883d0508d2f6c8405945c5d09f967985d5965749
Anuj Shakya <anuz@Anujs-MacBook-Air.local>
2023-06-04 17:01:37 +1000

    swipe menu implemented

--- council_app/www/js/index.js
+++ council_app/www/js/index.js
@@ -19,11 +19,82 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+// document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+// function onDeviceReady() {
+//     // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+//     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+//     document.getElementById('deviceready').classList.add('ready');
+// }
+
+
+$(document).ready(function() {
+	
+	$("#menuPanel").panel();
+	$('#menuListview').listview().listview('refresh');	
+
+  $("#img").click(function(){
+    $("#about-me").slideToggle("slow");
+  });
+
+ }
+ 
+); 
+
+
+$(document).on('pagecreate', '#home', function(event) {  
+    
+   $(".back").click(function(e) {
+	   // correct/preferred way to change a page 
+	   $("body").pagecontainer("change", "#home", { transition:'slide'});
+   });   
+
+
+// MENU --------------------------------
+
+   $(".menu").click(function() {
+         $("#menuPanel").panel("open");
+     }
+   );  	
+
+   $("#homeLink").click(function(e) {
+         $("body").pagecontainer("change", "#home",  {transition:"flip"});
+     }
+    );
+	
+   $("#logo").click(function(e) {
+	 $("body").pagecontainer("change", "#about", {transition:"slide"});
+     }
+   );
+
+// ------END MENU ----------
+
+        $(`#displayApiData`).click(function (){
+             fetchData();
+        }) //API function call
+        $(".clc").click(function(){
+            $("#display").val($("#display").val() +$(this).val());
+        });
+        $(".clear").click(function(){
+            $("#display").val('');
+        });
+        $(".result").click(function(){
+            $("#display").val(eval($("#display").val()));
+        });
+  }
+)
+// -----End of page create------
+
+
+$(document).on("swipeleft swiperight", function( e ) {
+        
+	if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swiperight"  ) {
+				  e.preventDefault();
+				$( "#menuPanel" ).panel( "open" );
+	    }
+	}
+ }
+);
+
