692d94b159430469285f83f08a183466a8ae4449
Rija Yousuf <rijayousuf@rijas-air.lan>
2023-06-05 23:31:37 +1000

    Fromt-end changes

--- council_app/www/js/index.js
+++ council_app/www/js/index.js
@@ -1,33 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-// document.addEventListener('deviceready', onDeviceReady, false);
-
-// function onDeviceReady() {
-//     // Cordova is now initialized. Have fun!
-
-//     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-//     document.getElementById('deviceready').classList.add('ready');
-// }
-
 
 $(document).ready(function() {
 	
@@ -38,9 +8,36 @@ $(document).ready(function() {
     $("#about-me").slideToggle("slow");
   });
 
+  $(function() {
+    // Open Popup
+    $('[popup-open]').on('click', function() {
+        var popup_name = $(this).attr('popup-open');
+ $('[popup-name="' + popup_name + '"]').fadeIn(300);
+    });
+ 
+    // Close Popup
+    $('[popup-close]').on('click', function() {
+ var popup_name = $(this).attr('popup-close');
+ $('[popup-name="' + popup_name + '"]').fadeOut(300);
+ window.location.reload();
+    });
+ 
+    // Close Popup When Click Outside
+    $('.popup').on('click', function() {
+ var popup_name = $(this).find('[popup-close]').attr('popup-close');
+ $('[popup-name="' + popup_name + '"]').fadeOut(300);
+ window.location.reload();
+    }).children().click(function() {
+ return false;
+    });
+ 
+});
+
  }
  
 ); 
+
+// End of document.ready function
 
 
 $(document).on('pagecreate', '#home', function(event) {  
